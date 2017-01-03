cdef4d7f1b371f6e9515ca7f1db7a4b1877d56fa
Patrick Watrous <patrickwatrous@live.com>
2017-01-03 01:33:26 +0000

    fixed bootstrap and added basic jquery

--- scripts.js
+++ scripts.js
@@ -1,2 +1,20 @@
 // API CALL
-// http://realtime.mbta.com/developer/api/v2/schedulebyroute?api_key=wX9NwuHnZU2ToO7GmGR9uw&route=orange&format
+// http://realtime.mbta.com/developer/api/v2/schedulebyroute?api_key=wX9NwuHnZU2ToO7GmGR9uw&route=orange&format=json
+
+$(document).ready(function() {
+  $(".submit").on("click", function() {
+    
+    var url = "http://realtime.mbta.com/developer/api/v2/schedulebyroute?api_key=wX9NwuHnZU2ToO7GmGR9uw&route=orange&format=json";
+
+    $.ajax({
+      type: "GET",
+      url: url,
+      async: false,
+      dataType: "json",
+      success: function(data) {
+        
+        }
+
+    });
+  });
+});
